fb093c523a194d44fec8cf1baae4054c9714be75
github9420 <wolf9420@gmail.com>
2018-06-05 10:30:58 +0800

    update version 1.0 2018/06/05

---
+++ Ubuntu.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+
+sudo apt-get install vim
+
+sudo apt-get install yakuake
+
+sudo apt-get install git
+sudo git config --global user.name "github9420"
+sudo git config --global user.email "wolf9420@gmail.com"
+
+
